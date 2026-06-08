a6dadc2a91e1ec78b5639c9df0c01e70c89118cf
Krishna Bharti <81293700+KrishnaBharti67@users.noreply.github.com>
2026-06-09 00:20:19 +0530

    Added streamlit dashboard

---
+++ Dashboard.py
@@ -0,0 +1,14 @@
+from pipeline import run_pipeline
+import streamlit as st
+import pandas as pd
+
+st.title("JUST TESITNG")
+x=st.multiselect("Ticker",["NVDA", "AAPL", "GOOG", "GOOGL", "MSFT", "AMZN", "AVGO", "META", "TSLA", "BRK.B",
+ "LLY", "WMT", "JPM", "V", "UNH", "XOM", "ORCL", "MA", "COST", "HD",
+ "PG", "NFLX", "JNJ", "BAC", "ABBV", "KO", "CRM", "CVX", "MRK", "AMD",
+ "TMO", "ACN", "PEP", "LIN", "MCD", "CSCO", "IBM", "GE", "ADBE", "TXN",
+ "PM", "CAT", "INTU", "DHR", "GS", "ISRG", "NOW", "AMGN", "SPGI", "BKNG"])
+
+if st.button("Analyse"):
+   score=run_pipeline(x)
+   st.bar_chart(score)
